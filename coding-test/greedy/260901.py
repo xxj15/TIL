678ac1d133f2b878a0e9bb27676a854607fb0ce2
xxj15 <seojini00@naver.com>
2026-09-01 18:08:27 +0900

    greedy 두 큐 합 같게 만들기

--- coding-test/greedy/260901.py
+++ coding-test/greedy/260901.py
@@ -1,10 +1,9 @@
 from collections import deque
 
 def solution(queue1, queue2):
+    n = (len(queue1)+len(queue2))*2
     queue1 = deque(queue1)
     queue2 = deque(queue2)
-
-    original = queue1.copy()
 
     cnt = 0
     sum1 = sum(queue1)
@@ -13,11 +12,11 @@ def solution(queue1, queue2):
     if (sum1+sum2)%2 :
         return -1 
     
-    while cnt < (len(queue1) + len(queue2))*4:
+    while True:
         if sum1 == sum2:
             return cnt
 
-        if cnt > 0 and queue1 == original:
+        if cnt >= n:
             return -1
 
         if sum1 > sum2:
